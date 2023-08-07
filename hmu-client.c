20efcbe6f037065ba522d2fc56aadbaffdf83004
Alex-Zeng-UofT <alex.zenggg@gmail.com>
2023-08-07 20:39:27 +0000

    parsing args

--- hmu-client.c
+++ hmu-client.c
@@ -14,5 +14,16 @@
 // cmdline reminder: IP-address, portnum, username, filename
 int main(int argc, char *argv[]) {
   // TODO
+
+  char *address = argv[1];
+  int port = atoi(argv[2]);
+  char *username = argv[3];
+  char *filename = argv[4];
+
+  printf("%s\n", address);
+  printf("%d\n", port);
+  printf("%s\n", username);
+  printf("%s\n", filename);
+
   return 0;
 }
